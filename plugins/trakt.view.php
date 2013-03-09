9e9d24e85d83d2aa7adbdbc0aab0f6a0a37eeeb7
Felix Arndt <mail@felixarndt.de>
2013-03-09 16:36:20 +0100

    trakt: add currently watching

---
+++ plugins/trakt.view.php
@@ -0,0 +1,3 @@
+<img src="<?= $this->poster; ?>" width="135" style="float: left;">
+<p><b><?= ($this->watching)? "Currently watching":"Last watched"; ?>: <i><?= $this->watched;?></i></b></p>
+<p>„<?= $this->title; ?>“<br>(S<?=$this->season;?>E<?=$this->episode;?>)</i></p>
